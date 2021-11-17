2f00d69bda62e5f7546b20a4491a9e32fc323945
Ghosh <Alisha_Ghosh@DellTeam.com>
2021-11-17 14:32:54 +0530

    add

---
+++ src/main/java/com/scott/betaexam/BetaExamApplication.java
@@ -0,0 +1,13 @@
+package com.scott.betaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaExamApplication.class, args);
+	}
+
+}
